9fa7cab38402da7dfbcc31eda0df28d3dd854757
iftykhar <s.m.ifty49@gmail.com>
2025-12-23 05:10:02 +0600

    feat: api intigrated to dashboardoverview and chart

---
+++ src/lib/types/analytics.ts
@@ -0,0 +1,8 @@
+// src/lib/types/analytics.ts
+
+   export interface Analytics{
+        productCount: number;
+        totalUser: number;
+        totalAmount: number;
+        totalProductSell: number;
+    }
