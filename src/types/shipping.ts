b9b3a329457d312e355541b49262adc01f376ed8
iftykhar <s.m.ifty49@gmail.com>
2026-01-16 04:51:05 +0600

    fix: showing shipping address info

--- src/types/shipping.ts
+++ src/types/shipping.ts
@@ -1,16 +1,34 @@
 export interface ShippingData {
   _id: string;
-  userId: string;
+  userId: {
+    _id: string;
+    firstName: string;
+    lastName: string;
+    email: string;
+  };
   orderId: string;
   fullName: string;
   email: string;
   phone: string;
+  company?: string;
   street: string;
   city: string;
   province: string;
   postalCode: string;
   country: string;
   deliveryInstructions?: string;
+  invoiceDetails?: {
+    name: string;
+    email: string;
+    phone: string;
+    company: string;
+    vat: string;
+    address: string;
+    city: string;
+    province: string;
+    postalCode: string;
+    country: string;
+  };
   createdAt: string;
   updatedAt: string;
 }
